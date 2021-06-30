d03f2daebea13442cb7fca16c7204b6ae42055cf
Cintyawardani <Cintyawardanii@gmail.com>
2021-06-30 15:01:49 +0800

    menambahkan database

---
+++ database/add.php
@@ -0,0 +1,12 @@
+<?php
+ include 'Conn.php';
+
+ $id_cust = $_POST['id_cust'];
+ $nama_cust= $_POST['nama_cust'];
+ $email_cust = $_POST['email_cust'];
+ $no_tlp_cust = $_POST['no_tlp_cust'];
+ $alamat_cust = $_POST['alamat_cust'];
+ $jenis_kelamin = $_POST['jenis_kelamin'];
+
+ $connect->query("INSERT INTO tabel_customer (id_cust,nama_cust,email_cust,no_tlp_cust,alamat_cust,jenis_kelamin) VALUES('".$itemcode."','".$id_cust."','".$nama_cust."','".$email_cust."','".$no_tlp_cust."','".$alamat_cust."','".jenis_kelamin."')")
+?>
